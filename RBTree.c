687ab9aedbfb71fbd94e6bde53517b6557f2ca3d
Dimanteo <dikolobay@gmail.com>
2021-02-17 01:59:24 +0300

    Started to implement Red-Black algorithm. Redo insert function. Added tree rotations.

--- RBTree.c
+++ RBTree.c
@@ -1,17 +1,29 @@
 #include "RBTree.h"
 
+enum Color {BLACK, RED};
+
+static const unsigned int NLeaves = 2u;
 static const int Left = 0;
 static const int Right = 1;
 
 struct RBTree {
         value_t value;
+        enum Color color;
         struct RBTree *children[2];
         struct RBTree *parent;
 };
 
-int rbt_isempty(struct RBTree *child)
+static enum Color get_color(struct RBTree *tree);
+
+static int balance(struct RBTree *node);
+
+static void rotate_left(struct RBTree *node);
+
+static void rotate_right(struct RBTree *node);
+
+int rbt_isempty(struct RBTree *leaf)
 {
-        if (child == NULL) {
+        if (leaf == NULL) {
                 return 1;
         } else {
                 return 0;
@@ -37,6 +49,7 @@ struct RBTree *rbt_init(value_t val)
         tree->children[Left] = NULL;
         tree->children[Right] = NULL;
         tree->value = val;
+        tree->color = BLACK;
 
         return tree;
 }
@@ -44,7 +57,7 @@ struct RBTree *rbt_init(value_t val)
 int rbt_destruct(struct RBTree *tree) 
 {
         assert(tree);
-        if (!tree) {
+        if (tree == NULL) {
                 return -1;
         }
 
@@ -64,7 +77,7 @@ int rbt_destruct(struct RBTree *tree)
 int rbt_insert(struct RBTree *tree, value_t val)
 {
         assert(tree);
-        if (!tree) {
+        if (tree == NULL) {
                 return -1;
         }
 
@@ -84,6 +97,8 @@ int rbt_insert(struct RBTree *tree, value_t val)
                 }
                 tree->children[child_index] = tmp;
                 tmp->parent = tree;
+                tmp->color = RED;
+                balance(tmp);
         } else {
                 int retcode = rbt_insert(tree->children[child_index], val);
                 return retcode;
@@ -171,7 +186,7 @@ int rbt_remove(struct RBTree *tree)
 struct RBTree *rbt_get_left(const struct RBTree *tree)
 {
         assert(tree);
-        if (!tree) {
+        if (tree == NULL) {
                 return NULL;
         }
 
@@ -181,7 +196,7 @@ struct RBTree *rbt_get_left(const struct RBTree *tree)
 struct RBTree *rbt_get_right(const struct RBTree *tree) 
 {
         assert(tree);
-        if (!tree) {
+        if (tree == NULL) {
                 return NULL;
         }
 
@@ -191,7 +206,7 @@ struct RBTree *rbt_get_right(const struct RBTree *tree)
 struct RBTree *rbt_get_parent(const struct RBTree *tree)
 {
         assert(tree);
-        if (!tree) {
+        if (tree == NULL) {
                 return NULL;
         }
 
@@ -205,7 +220,7 @@ struct RBTree *rbt_get_parent(const struct RBTree *tree)
 value_t rbt_get_val(const struct RBTree *tree, int* err) 
 {
         assert(tree);
-        if (!tree) {
+        if (tree == NULL) {
                 if (err != NULL) {
                         *err = 1;
                 }
@@ -216,4 +231,128 @@ value_t rbt_get_val(const struct RBTree *tree, int* err)
                 *err = 0;
         }
         return tree->value;
+}
+
+static enum Color get_color(struct RBTree *tree)
+{
+        assert(tree);
+        if (rbt_isempty(tree) || tree == NULL) {
+                return BLACK;
+        }
+
+        return tree->color;
+}
+
+static void rotate_left(struct RBTree *node)
+{
+        struct RBTree *pivot = rbt_get_right(node);
+        assert(pivot);
+
+        if (!rbt_isroot(node)) {
+                struct RBTree *parent = rbt_get_parent(node);
+                assert(parent);
+
+                if (parent->children[Left] == node) {
+                        parent->children[Left] = pivot;
+                } else {
+                        parent->children[Right] = pivot;
+                }
+        }
+
+        pivot->parent = node->parent;
+        node->parent = pivot;
+        node->children[Right] = rbt_get_left(pivot);
+        pivot->children[Left] = node;
+}
+
+static void rotate_right(struct RBTree *node)
+{
+        struct RBTree *pivot = rbt_get_left(node);
+        assert(pivot);
+
+        if (!rbt_isroot(node)) {
+                struct RBTree *parent = rbt_get_parent(node);
+                assert(parent);
+
+                if (parent->children[Left] == node) {
+                        parent->children[Left] = pivot;
+                } else {
+                        parent->children[Right] = pivot;
+                }
+        }
+
+        pivot->parent = node->parent;
+        node->parent = pivot;
+        node->children[Left] = rbt_get_right(pivot);
+        pivot->children[Right] = node;
+}
+
+static int balance(struct RBTree *node)
+{
+        assert(node);
+
+        struct RBTree *parent = NULL;
+        struct RBTree *uncle = NULL;
+        struct RBTree *granddad = NULL;
+
+        // case 1
+        if (rbt_isroot(node)) {
+                node->color = BLACK;
+                return 0;
+        }
+
+        parent = rbt_get_parent(node);
+        if (parent == NULL) {
+                return -1;
+        }
+
+        // case 2
+        if (get_color(parent) == BLACK) {
+                return 0;
+        }
+
+        granddad = rbt_get_parent(parent);
+        if (granddad == NULL) {
+                return -1;
+        }
+
+        /* looking for uncle
+         * and memorizing side of parent for case 4 */
+        int par_isleft = 0;
+        uncle = rbt_get_left(granddad);
+        if (uncle == parent) {
+                uncle = rbt_get_right(granddad);
+                par_isleft = 1;
+        }
+
+        /* case 3
+         * get_color doesn't throw errors
+         * at this point and further parent.color is RED,
+         * because otherwise it would be case 2 */
+        if (get_color(uncle) == RED) {
+                parent->color = BLACK;
+                uncle->color = BLACK;
+                granddad->color = RED;
+                return balance(granddad);;
+        }
+
+        /* case 4
+         * if uncle.color == BLACK */
+        if (par_isleft) {
+                if (rbt_get_right(parent) == node) {
+                        rotate_left(parent);
+                        node->color = BLACK;
+                        granddad->color = RED;
+                        rotate_right(granddad);
+                }
+        } else {
+                if (rbt_get_left(parent) == node) {
+                        rotate_right(parent);
+                        node->color = BLACK;
+                        granddad->color = RED;
+                        rotate_left(granddad);
+                }
+        }
+
+        return 0;
 }
